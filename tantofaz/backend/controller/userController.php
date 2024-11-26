95d230b7a710e60af5fc3a5982c6a24c4413e3ef
Enrique Sena <enriquesenacontato07@gmail.com>
2024-11-25 21:14:06 -0400

    feature: criando novas paginas.
    praticamente copiado todo das pagina de loginController.

---
+++ tantofaz/backend/controller/userController.php
@@ -0,0 +1,29 @@
+<?php
+include_once __DIR__ . "/../db/database.php";
+
+class LoginController{
+    private $conn;
+
+    public function __construct(){
+        $database = new Database();
+        $this->conn = $database->connect();
+
+
+    }
+    public function ValidaTudo($nome,$senha){
+        try {
+            $sql = "INSERT INTO usuario (nome,senha) VALUES(:nome,:senha)";
+            $db = $this->conn->prepare($sql);
+            $db->bindParam(":nome",$nome);
+            $db->bindParam(":senha",$senha);
+            if($db->execute){
+                return true;
+            }else{
+                return false;
+            }
+
+        } catch (\Exception $e) {
+            //throw $th
+        }
+        }
+    }    
