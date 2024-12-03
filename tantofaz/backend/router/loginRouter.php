3924cfe213051deb0ea5988e7bfbb46ad51c056c
Enrique Sena <enriquesenacontato07@gmail.com>
2024-12-02 21:00:40 -0400

    Feature: Organização
    Professor Rafael mostrou uma forma mais facil e clara de utilizar as paginas.

---
+++ tantofaz/backend/router/loginRouter.php
@@ -0,0 +1,13 @@
+<?php
+if($_SEVER["REQUEST_METHOD"] == "POST"){
+    // echo "teste:";
+    switch ($_GET ["acao"]){
+        case "validarLogin":
+            #code...
+            break;
+        default:
+            #code...
+            break;
+    }
+
+}
--- tantofaz/index.php
+++ tantofaz/index.php
@@ -3,41 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Document</title>
 </head>
 <body>
-    <form method="post">
-        <label >Nome</label>
-        <input type="text" name= "nome" id= "nome">
-        <label >Senha</label>
-        <input type="password" name= "senha" id= "senha">
-        <button type="submit">Entrar</button>
+    <form action="./backend/router/loginRouter.php?acao=validarlogin" method="POST">
+        <input type="text" name="nome">
+        <input type="password" name="senha">
+        <button type="submit">Logar</button>
+
     </form>
 </body>
 </html>
-<?php
-
-require_once __DIR__."/backend/controller/loginController.php";
-
-$loginController = new LoginController();
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $nome = $_POST['nome'];
-    $senha = $_POST['senha'];
-
-    if(empty($nome) || empty($senha)){
-        echo "Todos os campos são obrigatorios";
-    }else{
-        if($loginController-> ValidaTudo($nome,$senha)){
-            header("location: ./pages/home/index.php");
-            
-        }else{
-            echo"usuario invalido";
-        }
-        
-
-    }
-}
-?>
-
-<!-- http://localhost/tantofaz/AulasPHP/tantofaz/index.php -->
